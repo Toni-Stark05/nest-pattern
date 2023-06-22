031ce3f6ecedbc0f007834af2f7d748de7c760dd
tonistark <nikidysbdbdy@gmail.com>
2023-06-22 22:23:53 +0300

    add configModule & ref main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,37 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
+import * as path from 'path'
+
+import { ConfigModule } from '@nestjs/config'
+import appConfig from './config/app.config'
+
+function createEnvPath() {
+  let envFilePath = path.resolve(__dirname, '../environment/')
+
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      envFilePath += `/.env.production`
+      break
+    case 'local':
+      envFilePath += `/.env.local`
+      break
+    default:
+      envFilePath += `/.env.development`
+      break
+  }
+
+  return envFilePath
+}
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: createEnvPath(),
+      isGlobal: true,
+      load: [appConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-async function bootstrap() {
+async function start() {
+  const PORT = process.env.PORT || 3001
+  const PROCESS = process.pid
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(PORT, () => {
+    console.log('\x1b[32m', `[APP] ${PROCESS} -`, `App working from port`, '\x1b[33m', `${PORT}!`)
+  })
 }
-bootstrap()
+start()
